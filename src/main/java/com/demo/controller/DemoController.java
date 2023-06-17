40dc6951ada8cda687858dbe504085e3a643aae2
wanyutang <wanyutang@gmail.com>
2023-06-17 16:42:35 +0800

    test DemoTest convertAndSend OK

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -1,10 +1,12 @@
 package com.demo.controller;
 
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableJms
 @RestController
 public class DemoController {
     @PostMapping("demo1")
--- src/test/java/com/demo/DemoTest.java
+++ src/test/java/com/demo/DemoTest.java
@@ -2,18 +2,35 @@ package com.demo;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 
 @SpringBootTest(classes = {ApiApplication.class})
+@EnableJms
 @Slf4j
 public class DemoTest {
 
     @Value("${info.demo.val1}")
     String infoDemoVal1;
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
     @Test
-    public void demo() {
+    void demo() {
        log.debug("infoDemoVal1 log res: {}", infoDemoVal1);
     }
+
+    @Test
+    void convertAndSend() {
+        String message = "demo test msg";
+        jmsTemplate.convertAndSend("DEV.QUEUE.1", message);
+        log.debug("log res: {}","Message sent successfully");
+    }
+
 
 }
