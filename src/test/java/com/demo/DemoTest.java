19f8c3a90cc3e095167d1799f018bb92d21cbde4
wanyutang <wanyutang@gmail.com>
2023-06-17 17:36:33 +0800

    edit setObjectProperty

--- src/test/java/com/demo/DemoTest.java
+++ src/test/java/com/demo/DemoTest.java
@@ -4,11 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jms.JmsException;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
+
+import javax.jms.BytesMessage;
+import javax.jms.Session;
 
 @SpringBootTest(classes = {ApiApplication.class})
 @EnableJms
@@ -27,10 +32,22 @@ public class DemoTest {
 
     @Test
     void convertAndSend() {
-        String message = "demo test msg";
-        jmsTemplate.convertAndSend("DEV.QUEUE.1", message);
+        String xmlMessage = "<Message><Text>demo test msg</Text></Message>";
+        byte[] binaryData = xmlMessage.getBytes();
+        jmsTemplate.convertAndSend("DEV.QUEUE.1", binaryData);
         log.debug("log res: {}","Message sent successfully");
     }
 
+    @Test
+    void setObjectProperty() {
+        jmsTemplate.send("DEV.QUEUE.1", new MessageCreator() {
+            @Override
+            public BytesMessage createMessage(Session session) throws javax.jms.JMSException {
+                BytesMessage message = session.createBytesMessage();
+                message.setObjectProperty("JMS_IBM_MQMD_MsgId", "demo123");
+                return message;
+            }
+        });
+    }
 
 }
